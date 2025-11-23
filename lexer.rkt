cc773c71587ef361d2f8348d730e2bba7db694f1
lillie <onlinechronically@protonmail.com>
2025-11-23 16:41:17 -0500

    Initial Commit+Lexer

---
+++ lexer.rkt
@@ -0,0 +1,90 @@
+#lang racket
+
+(require "structs.rkt")
+
+(define ops
+  (list
+   '+
+   '-
+   '*
+   '=
+   '!
+   '|(|
+   '|)|
+   '|{|
+   '|}|))
+
+(define char-ops (map (λ (sym-op) (string-ref (symbol->string sym-op) 0)) ops))
+
+(define list-op?
+  (λ (loc) (op? (string->symbol (list->string loc)))))
+
+(define op?
+  (λ (op) (memv op ops)))
+
+(define word-char?
+  (λ (c)
+    (not (member c char-ops))))
+
+(define value->lexeme
+  (λ (s)
+    (match s
+      [n #:when (number? n) (Number n)]
+      [b #:when (boolean? b) (Boolean (if b #t #f))]
+      ['\; (Semicolon)]
+      [(? op?) (Symbol s)]
+      [v #:when (symbol? v)
+         (match v
+           [(or 'true 'false) (Boolean (eqv? v 'true))]
+           ['let (Keyword 'Let)]
+           ['if (Keyword 'If)]
+           ['else (Keyword 'Else)]
+           [_ (Identifier v)])]
+      [_ (error "invalid char found")])))
+
+(define char->number
+  (λ (c) (- (char->integer c) (char->integer #\0))))
+
+(define word?
+  (λ (loc)
+    (cond
+      [(list? loc)
+       (let* [(str (list->string loc))
+              (sym (string->symbol str))]
+         (cond
+           [(ormap op? loc) #f]
+           [(memv sym ops) #f]
+           [(= (string-length str) 0) #t]
+           [else #t]))]
+      [else #f])))
+
+(define acc->sym
+  (λ (acc)
+    (match acc
+      ['|;| acc]
+      [(? number?) acc]
+      [(or (? list-op?) (? word?)) (string->symbol (apply string acc))])))
+
+(define extend-acc
+  (λ (acc item)
+    (append acc (if (null? item) null (list (acc->sym item))))))
+
+(define file->symbols
+  (λ (p acc alt-acc)
+    (let [(r (read-char p))]
+      (match* (r alt-acc)
+        [(_ _) #:when (eof-object? r) (extend-acc acc alt-acc)]
+        [(#\; _) (file->symbols p (extend-acc (extend-acc acc alt-acc) '|;|) null)]
+        [(#\space _) (file->symbols p (extend-acc acc alt-acc) null)]
+        [(#\newline _) (file->symbols p acc alt-acc)]
+        [((? char-numeric?) (? number?)) (file->symbols p acc (+ (* alt-acc 10) (char->number r)))]
+        [((? char-numeric?) _) (file->symbols p (extend-acc acc alt-acc) (char->number r))]
+        [((? word-char?) (? word?)) (file->symbols p acc (append alt-acc (list r)))]
+        [((? word-char?) _) (file->symbols p (extend-acc acc alt-acc) (list r))]
+        [((? char?) (? op?)) (file->symbols p acc (append alt-acc (list r)))]
+        [((? char?) _) (file->symbols p (extend-acc acc alt-acc) (list r))]))))
+
+(define lex
+  (λ (file-name) (map value->lexeme (file->symbols (open-input-file file-name) null null))))
+
+(provide lex)
---
+++ structs.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+;; Lexer
+(define-struct Keyword (keyword) #:transparent)
+(define-struct Identifier (ident) #:transparent)
+(define-struct Symbol (sym) #:transparent)
+(define-struct Semicolon () #:transparent)
+
+;; Intermediate Representations
+(define-struct If (cnd body) #:transparent)
+(define-struct Else (body) #:transparent)
+
+;; Parser Add-ons
+(define-struct Begin (stmts final) #:transparent)
+(define-struct Number (n) #:transparent)
+(define-struct Boolean (b) #:transparent)
+(define-struct Let (sym rhs body) #:transparent)
+(define-struct Void () #:transparent)
+(define-struct SetBang (sym rhs) #:transparent)
+(define-struct Unary (op child) #:transparent)
+(define-struct Binary (op lhs rhs) #:transparent)
+(define-struct WhileLoop (cnd body) #:transparent)
+(define-struct IfStmt (cnd conseq alt) #:transparent)
+
+(provide (struct-out Keyword) (struct-out Identifier) (struct-out Symbol)
+
+         (struct-out Begin) (struct-out Number) (struct-out Boolean)
+         (struct-out Semicolon) (struct-out Let) (struct-out Void)
+         (struct-out SetBang) (struct-out Unary) (struct-out Binary)
+         (struct-out WhileLoop) (struct-out If) (struct-out Else)
+         (struct-out IfStmt))
